e2853d0fa1f7067777cf1aea4a889bcc31ffcc55
Jake Elder <jake@jakes.world>
2020-12-11 09:57:38 +0700

    feat: share TS config

--- packages/project-eden-ui/stories/Person.stories.tsx
+++ packages/project-eden-ui/stories/Person.stories.tsx
@@ -5,6 +5,6 @@ import Person, { Props as PersonProps } from "../src/components/Person";
 export default {
   title: "Components/Person",
   component: Person,
-} as Person;
+} as Meta;
 
 export const Default: Story<PersonProps> = (args) => <Person {...args} />;
