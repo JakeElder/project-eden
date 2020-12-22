cfe8483492e4b954e7e3b4b40cfd5f87afa7f57d
Jake Elder <jake@jakes.world>
2020-12-22 20:41:25 +0700

    chore: remove Person component

--- packages/project-eden-ui/src/index.ts
+++ packages/project-eden-ui/src/index.ts
@@ -1,8 +1,3 @@
-export { default as Theme } from "./components/Theme";
-
-export { default as Person } from "./components/Person";
-export type { Props as PersonProps } from "./components/Person";
-
 export { default as Heading } from "./components/Heading";
-
 export { default as TakeoverVideo } from "./components/TakeoverVideo";
+export { default as Theme } from "./components/Theme";
